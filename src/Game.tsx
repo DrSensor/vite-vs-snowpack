10edb1bcae8dd779d602b764de2a3d52516d9c43
Fahmi Akbar Wildana <f.a.wildana@gmail.com>
2021-01-30 23:51:07 +0700

    (╯°□°)╯︵ ┻━┻

---
+++ src/Game.tsx
@@ -0,0 +1,25 @@
+import React, { } from "react";
+import { Engine, Scene } from "react-babylonjs";
+import { Vector3 } from "@babylonjs/core";
+
+import Mesh from "./Mesh";
+
+export default () => {
+
+    return <Engine antialias adaptToDeviceRatio>
+        <Scene>
+            <freeCamera name="main_camera"
+                position={new Vector3(0, 5, -10)}
+                setTarget={[Vector3.Zero()]} />
+
+            <hemisphericLight name="light1"
+                direction={Vector3.Up()} />
+
+            <Mesh />
+
+            <ground name="ground"
+                width={6} height={6}
+                subdivisions={2} />
+        </Scene>
+    </Engine>
+}
---
+++ src/Mesh.tsx
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import { useClick } from "react-babylonjs";
+import { Vector3, Color3 } from "@babylonjs/core";
+
+export default () => {
+    const [color, setColor] = useState(Color3.Random())
+    const [ref] = useClick(_ => {
+        setColor(Color3.Random())
+    })
+
+    return <sphere name="ball" ref={ref}
+        position={Vector3.Up()}
+        diameter={2} segments={32}
+    >
+        <standardMaterial name="mat" diffuseColor={color} />
+
+    </sphere>
+
+}
